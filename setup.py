3fc8aa7e1e9cb8b995b7dfe9a482e5e523cece42
dmux <rafael.sales@gmail.com>
2020-11-20 01:09:00 +0000

    many fix

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
                                                os.path.join('schema', 'pl_006s', '*')],
                   'pysignfe.nfe.manual_500':  [os.path.join('schema', 'pl_008e', '*')],
                   'pysignfe.nfe.manual_600':  [os.path.join('schema', 'pl_008i2', '*')],
+                  'pysignfe.nfe.manual_700':  [os.path.join('schema', 'PL_009_V4', '*')],
                   'pysignfe.xml_sped':  [os.path.join('schema', '*')],
                   'pysignfe.nfse': ['certificados/*'],
                   'pysignfe.nfse.bhiss': [os.path.join('v10', 'schema', '*')]
